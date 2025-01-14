d80439fb344420aa8fc5288a36b7c8dbd6ebe9c5
NareshKumar-own-S <nareshksarangan@gmail.com>
2025-01-14 23:05:28 +0530

    added skill and encrypition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import AppPage from "@/components/appPage/AppPage";
+import Encryption from "@/components/encryption/Encryption";
+import Skills from "@/components/skills/Skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <AppPage />
+        <Skills/>
+        <Encryption/>
       </div>
     </main>
   );
--- src/components/appPage/AppPage.tsx
+++ src/components/appPage/AppPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HeroContent from '../dashboard/DashBoardContent';
+import DashBoardContent from '../dashboard/DashBoardContent';
 import SpaceVideo from '../video/SpaceVideo';
 
 const AppPage = () => {
   return (
     <div className="relative flex flex-col h-full w-full" id="about-me">
       <SpaceVideo />
-      <HeroContent />
+      <DashBoardContent />
     </div>
   );
 };
